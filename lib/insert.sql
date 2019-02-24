15b64af2a713058d112c556fd72e3f6d4de2c78b
campbelllssoup <matjocampbell@gmail.com>
2019-02-24 17:55:10 -0500

    Created bears data table with corresponding data & completed exercises querying the bears table

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Mr.Chocolate", 3, "Male", "Brown", "Mean", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Rowdy", 7, "Male", "Black", "Aggressive", 0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Tabitha", 5, "Female", "Black", "Passive Aggressive", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Sergeant Brown", 10, "Female", "Blonde", "Aggressive", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Melissa", 2, "Female", "Red", "Passive", 0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Grinch", 4, "Male", "Red", "Passive Aggressive", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Wendy", 8, "Female", "Blonde", "Passive", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL, 10, "Female", "Brown", "Aggressive", 0);
