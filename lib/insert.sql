16264a385763ebf286f7d318052462922d2b07aa
WSINTRA <will.simpson85@gmail.com>
2019-02-25 12:03:35 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 3, 'female', 'blue', 'Chilled', 'false'),
+('Rowdy', 3, 'female', 'blue', 'Chilled', 'false'),
+('Tabitha', 3, 'female', 'blue', 'Chilled', 'false'),
+('Sergeant Brown', 3, 'female', 'blue', 'Chilled', 'false'),
+('Melissa', 3, 'female', 'blue', 'Chilled', 'false'),
+('Grinch', 3, 'female', 'blue', 'Chilled', 'false'),
+('Wendy', 3, 'female', 'blue', 'Chilled', 'false'),
+(NULL, 3, 'female', 'blue', 'Chilled', 'false');
+
